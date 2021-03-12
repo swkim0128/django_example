4fab14341aa0b73237cd3da62b7b12de1b518198
eunsol <swkim0128@github.com>
2021-03-12 21:57:47 +0900

    BUG: modify settings
    gitignore 관련 충돌로 인한 연결 오류 해결
    기존에 세팅하였던 settings 파일이 수정이 되어있던 상태여서 수정을 함
    추가적으로 account 관련 기능 접속 성공함.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,45 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+from .models import Profile
 class SignupForm(UserCreationForm) :
-    pass
+    username = forms.CharField(label='사용자명', widget=forms.TextInput(attrs={
+        'pattern': '[a-zA-Z0-9]+',
+        'title': '특수문자, 공백 입력불가',
+    }))
+
+    nickname = forms.CharField(label='닉네임')
+    picture = forms.ImageField(label='프로필 사진', required=False)
+
+    class Meta(UserCreationForm.Meta) :
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+        def clean_nickname(self) :
+            nickname = self.cleaned_data.get('nickname')
+            if Profile.objects.filter(nickname=nickname).exists():
+                raise forms.ValidationError('이미 존재하는 닉네임 입니다.')
+            return nickname
+
+        def clean_email(self) :
+            email = self.cleaned_data.get('email')
+            User = get_user_model()
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('사용중인 이메일입니다.')
+            return email
+
+        def clean_picture(self) :
+            picture = self.clean_data.get('picture')
+            if not picture :
+                picture = None
+            return picture
+
+        def save(self) :
+            user = super().save()
+            Profile.objects.create(
+                user=user,
+                nickname=self.cleaned_data['nickname'],
+                picture=self.cleaned_data['picture'],
+            )
+            return user
